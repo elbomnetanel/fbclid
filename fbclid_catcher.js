1e0febecc7ce8f3237648e93a66dc9531f203b3e
alipetarian <alipakistani90@gmail.com>
2021-09-17 23:59:03 +0500

    Update fbclid_catcher.js

--- fbclid_catcher.js
+++ fbclid_catcher.js
@@ -12,3 +12,44 @@ var fbp = cookies._fbp
 var ua = window.navigator.userAgent
 history.pushState({}, null, queryString+'&fbclid='+fbclick_final+'&fbp='+fbp+'ua'+ua);
 }
+
+
+For Google Tag manaer 
+
+<script>
+  
+if (window.location.href.indexOf('fbclid') > 0) {
+  console.log('Clickid is present url '+ window.location.href.indexOf('fbclid'))
+  
+} else {
+  var queryString = window.location.search;
+  var allCookies = document.cookie.split(';').map(function (cookie) {
+    return cookie.split('=');
+  })
+  .reduce(function (cookies, line) {
+    cookies[line[0].trim()] = line[1]
+    return cookies;
+    }, {});
+  
+  
+  console.log('ALL COOKIES ++++++++++++++++++++', allCookies)
+  
+  var fbclick = allCookies._fbc;
+  console.log('fbclick', fbclick)
+  
+  var fbclick_final = fbclick.slice(19);
+  
+   console.log('fbclick_final', fbclick_final)
+  var fbp = allCookies._fbp;
+  var ua = window.navigator.userAgent;
+  
+  var fbclid= fbclick_final+'&fbp='+fbp+'ua'+ua
+  
+ console.log('FINAL FBCLID', fbclid)
+history.pushState({}, null, queryString+'&fbclid='+fbclid);
+}
+</script>
+
+
+
+
